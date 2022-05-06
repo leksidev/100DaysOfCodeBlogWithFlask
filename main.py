138656856e28b52d02d264b32a27cbd559ef6ba3
Oleksa <petrova.alexxandra@gmail.com>
2022-05-07 01:12:57 +0300

    Made config postresql

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ gravatar = Gravatar(app, size=100,
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
